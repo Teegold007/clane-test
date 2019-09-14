00c6e367b72109989226b004e10b8a1370f81283
sokunniga <sokunniga@teamapt.com>
2019-09-14 02:08:16 +0100

    Add seeder for users and article

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $user =App\User::create(['id'=> '1','name' => 'Ben', 'email' => 'clane@test.com',
+            'password' => password_hash('123456', PASSWORD_BCRYPT)]);
+
+        factory(App\User::class, 10)->create();
+    }
+}
